37d489086d38cbd0b18b8e2e5ba94cc2054ac12b
Azeem-dev14 <azeem.dev14@gmail.com>
2026-07-02 10:48:28 +0530

    Initial Commit

---
+++ vitawinTests/vitawinTests.swift
@@ -0,0 +1,17 @@
+//
+//  vitawinTests.swift
+//  vitawinTests
+//
+//  Created by Azeem on 02/07/26.
+//
+
+import Testing
+@testable import vitawin
+
+struct vitawinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
